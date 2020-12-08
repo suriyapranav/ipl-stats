c48fd545a66e6e694c81f511560ac9312efdafa4
Suriya Pranav <50844429+suriyapranav@users.noreply.github.com>
2020-12-08 12:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,9 @@ def reset():
 	data.show=False
 	
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
